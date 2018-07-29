45bab268e6df93dbea97200c681b2bdb64581a26
trung <>
2018-07-29 23:36:39 +0700

    Enable https, redirect http to https and redirect www to non-www

--- eos-tracker-frontend/server.js
+++ eos-tracker-frontend/server.js
@@ -1,8 +1,47 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('*', function(req,res) {
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var httpsServer = require('https');
+var httpServer = require('http');
+const fs = require('fs');
+
+var httpsApp = express();
+var httpApp = express();
+
+const httpsOpt = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/feesimplewallet.io-0002/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/feesimplewallet.io-0002/privkey.pem')
+};
+
+httpsApp.set('port', 443); // default port for https is 443
+
+httpApp.set('port', 80); // default port for http is 80
+httpApp.get("*", function (req, res, next) {
+	let host = req.headers.host;
+	if (host.match(/^www/) !== null ) {
+    host = host.replace(/^www\./, '');
+  }	
+	res.redirect("https://" + host + req.path);
+});
+
+//Middleware
+httpsApp.use(bodyParser.json());
+httpsApp.use(bodyParser.urlencoded({extended: false}));
+
+//Prevent click jacking
+httpsApp.use(require('helmet')());
+
+//Set static path for assets
+httpsApp.use(express.static(__dirname + '/dist'));
+
+httpsApp.get('*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+httpServer.createServer(httpApp).listen(httpApp.get('port'), function() {
+	console.log('Express HTTP server listening on port ' + httpApp.get('port'));
+});
+
+httpsServer.createServer(httpsOpt, httpsApp).listen(httpsApp.get('port'), function() {
+	console.log('Express HTTPS server listening on port ' + httpsApp.get('port'));
+});
