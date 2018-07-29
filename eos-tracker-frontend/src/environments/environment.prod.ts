87a04def86079c2bea43ace2c5029dc18001d28c
trung <server@feesimple.io>
2018-07-29 16:18:54 +0000

    Add config file for tracker-frontend when build with product mode

--- eos-tracker-frontend/src/environments/environment.prod.ts
+++ eos-tracker-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,14 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
   production: true,
-  walletUrl: 'https://walleteos.com',
+  walletUrl: 'http://138.197.194.220:3000',
   votingUrl: 'https://eosportal.io',
   appName: 'XFS Tracker',
   logoUrl: '/assets/logo.png',
-  apiUrl: 'https://api.eostracker.io',
-  blockchainUrl: 'https://api1.eosdublin.io'
+  apiUrl: 'http://138.197.194.220:4201',
+  blockchainUrl: 'http://138.197.194.220:8877'
 };
