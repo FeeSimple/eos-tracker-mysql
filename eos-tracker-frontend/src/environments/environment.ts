b20b5c6fbc786a5ff5b90edecaea468ea94e8ac5
trung <server@feesimple.io>
2018-08-05 19:06:35 +0000

    Reconfigure frontend to new port for https

--- eos-tracker-frontend/src/environments/environment.ts
+++ eos-tracker-frontend/src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
   appName: 'XFS Tracker',
   logoUrl: '/assets/logo.png',
   apiUrl: 'https://feesimpletracker.io:444',
-  blockchainUrl: 'https://138.197.194.220:8877',
+  blockchainUrl: 'https://feesimpletracker.io:8878',
  chainId: '1c6ae7719a2a3b4ecb19584a30ff510ba1b6ded86e1fd8b8fc22f1179c622a32'
 };
